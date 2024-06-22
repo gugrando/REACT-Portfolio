be418367a7005a63916c0b79be9359cd40e0aa35
gugrando <gustavo.vieira0909@gmail.com>
2024-06-22 01:45:00 -0300

    Alpha Version 2

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import Home from './sections/Home'
 import Techs from './sections/Techs'
 import About from './sections/About'
-import DarkButton from './Dark'
+// import DarkButton from './Dark'
 import Projects from './sections/Projects'
 import Contact from './sections/Contact'
 import ContentContainer from './ContentContainer'
